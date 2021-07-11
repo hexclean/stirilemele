bcd0b697430ce26502a5ab9a8bd105cf18189d55
hexclean <erdosjozsef20@gmail.com>
2021-07-12 01:38:17 +0300

    add dynamic content of title

--- admin/routes/other.js
+++ admin/routes/other.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const dynamicLinkController = require("../controllers/dynamicLinks/dynamicLinks");
 const otherController = require("../controllers/other");
 const sourceController = require("../controllers/source");
+const isAuth = require("../../middleware/is-auth");
 
 // Főoldal
 router.get("/categorii", otherController.getCategoryScreen);
 router.get("/top-stiri", otherController.getTopArticles);
 router.get("/comment-email", otherController.getTopArticles);
-router.get("/stiri-configurate", otherController.getConfiguredHome);
+router.get("/stiri-configurate", isAuth, otherController.getConfiguredHome);
 router.get("/important", otherController.getFontos);
 router.get("/help", otherController.getBetekintes);
 module.exports = router;
