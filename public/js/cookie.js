3e3cb1e793bf790f7fa97e993e731ce331c4692e
hexclean <erdosjozsef20@gmail.com>
2021-07-15 22:55:27 +0300

    cookie done

--- public/js/cookie.js
+++ public/js/cookie.js
@@ -1,6 +1,9 @@
 window.onload = function () {
   var acceptCookie = document.getElementById("acceptCookie");
+  var cookieBox = document.getElementById("cookieBox");
   acceptCookie.onclick = function () {
+    console.log("dasd");
     Cookies.set("cookie", true, { expires: 365 });
+    cookieBox.style.display = "none";
   };
 };
