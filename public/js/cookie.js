848c1ad6b37345ba539a2c67252c30319bd3aa48
hexclean <erdosjozsef20@gmail.com>
2021-07-20 22:37:35 +0300

    termeni

--- public/js/cookie.js
+++ public/js/cookie.js
@@ -2,7 +2,6 @@ window.onload = function () {
   var acceptCookie = document.getElementById("acceptCookie");
   var cookieBox = document.getElementById("cookieBox");
   acceptCookie.onclick = function () {
-    console.log("dasd");
     Cookies.set("cookie", true, { expires: 365 });
     cookieBox.style.display = "none";
   };
